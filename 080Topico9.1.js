2c1d1f9e074bfa80cd32526ea440f6c81e551991
Isaac <Isaac.macedo@yahoo.com.br>
2020-07-27 20:10:24 -0300

    inicio formulario

---
+++ 080Topico9.1.js
@@ -0,0 +1,38 @@
+<script>
+function mostra(frase){
+    document.write(frase + "<br>")
+}
+
+//window.onload = function(){
+//mostra("pagina carregou")}
+
+
+var dados = [];
+    dados.sort(function(a,b){
+        return a - b;
+})
+
+function msg(){
+    mostra('Adicionado');
+}
+mostra('<h3>Criar array de contatos.</h3>')
+
+var idadeDigitada = document.getElementsByClassName("enviar").value;
+
+function botaoClicado (){
+        if (idadeDigitada >= 100 || idadeDigitada <= 0 ){
+    alert("Numero Invalido : Idade deve ser maior que 0 e menor que 100.")
+        }  
+}
+
+var resposta = document.getElementById("resposta");
+//resposta.innerHTML = botaoClicado;   
+
+dados.push(idadeDigitada); //Adiciona os dados digitados na Array.
+
+var adicionarDados = document.getElementById("enviar");
+    adicionarDados.onclick = botaoClicado;
+
+mostra('Dados Digitados : ' + dados)
+
+</script>
