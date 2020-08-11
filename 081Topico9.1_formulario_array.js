1828b25f202bc0371c7030fe22eb9acefcaf2fd9
Isaac <Isaac.macedo@yahoo.com.br>
2020-08-11 20:17:50 -0300

    formulario com array

---
+++ 081Topico9.1_formulario_array.js
@@ -0,0 +1,25 @@
+function mostra(frase){
+    document.write(frase + '<br>')
+}
+
+var contatos = new Array(4);
+    for (let i = 0; i < 3; i++) {
+        console.log(contatos[i] = i);
+    }
+
+var teste = ['nome', 'idade', 'email'];
+console.log('Array com Join : ' + teste.join(' '));
+console.log('Array com Sort : ' + teste.sort());
+
+var contatos = [
+    {nome: ""},
+    {idade: "" },
+    {email: "" }
+];
+
+contatos.nome = "joao";
+console.log(contatos.nome)
+
+function adicionar () {
+
+}
