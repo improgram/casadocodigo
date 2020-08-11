064f8de8e24014c61f9cc82431dae7574c95d3c7
Isaac <Isaac.macedo@yahoo.com.br>
2020-08-11 19:07:31 -0300

    Capitulo 9.1 formulario Html

--- 080Topico9.1.js
+++ 080Topico9.1.js
@@ -1,42 +1,23 @@
 function mostra(frase){
     document.write(frase + "<br>")
 }
-mostra("<h4>Javascript carregou.</h4>")
-var contatos = [];
-
 function capturar() {
     var data = new Date();
     console.log(data.toString())
 
-    contatos = document.getElementById('nome').value;
-    document.getElementById('dadosDigitados').innerText = contatos;
-
-    //contatos = document.getElementsByTagName("input").value;
-    //document.getElementById('dadosDigitados').innerText = contatos;
-
     var nome = document.getElementById('nome').value;
+        nome = document.getElementById('nome').value;
+            document.getElementById('nomeDigitado').innerText = nome
+
     var idade = document.getElementById('idade').value;
+        idade = document.getElementById('idade').value;
+            document.getElementById('idadeDigitado').innerHTML = idade;
+    
     var email = document.getElementById('email').value;
+        email = document.getElementById('email').value;
+            document.getElementById('emailDigitado').innerHTML = email;
+
     var assunto = document.getElementById('assunto').value;
-    console.log(assunto);
-}    
-
-
-    console.log(
-        //document.querySelector('#formulario').innerText
-        //document.querySelector('input[name="nome"]').innerText
-        //document.getElementsByClassName("meuForm")
-        //document.getElementsByTagName("input")['nome'].textContent
-        //document.getElementsByTagName('#input').innerText;
-        //document.getElementById("formulario").getElementsByTagName("#input").innerText
-    )    
-
-//test
-//https://js.do/marcosr
-
-
-contatos.push("Joao" , " 22" , "email@email.com" , "Vote Nulo");
-mostra('<strong>Nome: </strong>' + contatos[0])
-mostra('<strong>Idade : </strong>' + contatos[1] + ' anos')
-mostra('<strong>Email : </strong>' + contatos[2])
-mostra('<strong>Assunto : </strong>' + contatos[3])
+        assunto = document.getElementById('assunto').value;
+            document.getElementById('assuntoDigitado').innerText = assunto;
+}
