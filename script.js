165f8aa4605ed208f843c233a5d5539465eb8590
Isaac <email.email.com>
2021-01-11 20:33:47 -0300

    vinculado resultado solicitado abaixo do button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let cores=["#4e84ad", "#5E610B", "#314250", "#FE642E","#0B3B0B","#0A122A", "#0B3B39"];
+let cores=["##01A9DB", "#086A87", "##0489B1", "#0B3B39", "#4e84bc", "#314250", "#0B3861"];
 let i=0;
 let timer=setInterval(mudaCorFundo,3000);
 	function mudaCorFundo(){
@@ -15,23 +15,38 @@ function batimentos () {
     console.log('Anos de vida: ' + idade);
     let diasdevida = idade * 365;
     console.log('Dias de vida: ' + diasdevida);
-    let batimentos = 'Seu coração bateu: ' + diasdevida * 24 * 60 * 80;
-    console.log(batimentos);
-    document.getElementById("result").innerHTML = batimentos;
-    document.getElementById("result").style="background-color: #0004"
+    let calcBatimentos = '<br>Idade: ' + idade + '<br>Seu coração bateu: ' + diasdevida * 24 * 60 * 80 + " vezes em toda sua vida.";
+    console.log(calcBatimentos);
+    let batimentos = document.getElementById("batimentos");
+
+    //batimentos.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
+    //https://pixabay.com/images/id-3312513/
+    //https://pixabay.com/images/id-961194/
+    //https://pixabay.com/images/id-157895/
+    //https://pixabay.com/images/id-163708/
+    //https://pixabay.com/images/id-2069872/
+    batimentos.height = "100";
+    batimentos.insertAdjacentHTML("afterend", calcBatimentos);
+    //document.getElementById("result").style="background-color: #0004"
 }
 
 // Anos de Copa
 function copas() {
     let anoCopa = 1930;
     let anoLimite = parseInt(prompt('Qual o ano limite na consulta?'))
-    let result = document.getElementById("result");
-    while(anoCopa <= anoLimite && anoCopa <= 2018){ 
+    //let result = document.createElement("div");
+    while(anoCopa <= anoLimite && anoCopa <= 2018){
         console.log(anoCopa);
-        var para = document.createElement('p');
-        para.textContent = "Em: " + anoCopa + " teve Copa do Mundo.";
         anoCopa = anoCopa + 4;
+        if (anoCopa = 1942 || anoCopa == 1946) {
+            var para = document.createElement('p');
+            para.textContent = "Em: " + anoCopa + " não teve Copa devido a Guerra Mundial.";
+        } else {
+            var para = document.createElement('p');
+            para.textContent = "Em: " + anoCopa + " teve Copa do Mundo.";
+        }
         result.appendChild(para);
+        break;
     }
 }
 
