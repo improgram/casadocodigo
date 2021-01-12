b47cb9718bffa1da0356d2afc85cb4573638fd2f
Isaac <email.email.com>
2021-01-12 20:34:08 -0300

    Adicionado figura no primeiro script

--- script.js
+++ script.js
@@ -18,34 +18,36 @@ function batimentos () {
     let calcBatimentos = '<br>Idade: ' + idade + '<br>Seu coração bateu: ' + diasdevida * 24 * 60 * 80 + " vezes em toda sua vida.";
     console.log(calcBatimentos);
     let batimentos = document.getElementById("batimentos");
-
-    //batimentos.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
+    batimentos.insertAdjacentHTML("afterend", calcBatimentos);
+    let img = document.createElement("img");
+    img.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
+    document.getElementById("batimentos").appendChild(img);//Adiciona imagem como filha do id=batimentos.
     //https://pixabay.com/images/id-3312513/
     //https://pixabay.com/images/id-961194/
     //https://pixabay.com/images/id-157895/
     //https://pixabay.com/images/id-163708/
     //https://pixabay.com/images/id-2069872/
-    batimentos.height = "100";
-    batimentos.insertAdjacentHTML("afterend", calcBatimentos);
-    //document.getElementById("result").style="background-color: #0004"
+    img.height = "100"; 
+    //document.getElementById("batimentos").style="background-color: #F6CECE";
 }
 
 // Anos de Copa
 function copas() {
     let anoCopa = 1930;
     let anoLimite = parseInt(prompt('Qual o ano limite na consulta?'))
-    //let result = document.createElement("div");
+    let copas = document.getElementById("copas");
     while(anoCopa <= anoLimite && anoCopa <= 2018){
         console.log(anoCopa);
         anoCopa = anoCopa + 4;
         if (anoCopa = 1942 || anoCopa == 1946) {
             var para = document.createElement('p');
             para.textContent = "Em: " + anoCopa + " não teve Copa devido a Guerra Mundial.";
+            continue
         } else {
             var para = document.createElement('p');
             para.textContent = "Em: " + anoCopa + " teve Copa do Mundo.";
         }
-        result.appendChild(para);
+        copas.appendChild(para);
         break;
     }
 }
