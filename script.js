d023ba66c746a4ef8d088d422f7d564945d3d297
Isaac <email.email.com>
2021-01-13 21:20:08 -0300

    adicionado script copa do mundo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let cores=["##01A9DB", "#086A87", "##0489B1", "#0B3B39", "#4e84bc", "#314250", "#0B3861"];
 let i=0;
-let timer=setInterval(mudaCorFundo,3000);
+let timer=setInterval(mudaCorFundo,5000);
 	function mudaCorFundo(){
 		document.body.style.backgroundColor=cores[i];
 		i++;
@@ -19,6 +19,7 @@ function batimentos () {
     console.log(calcBatimentos);
     let batimentos = document.getElementById("batimentos");
     batimentos.insertAdjacentHTML("afterend", calcBatimentos);
+    
     let img = document.createElement("img");
     img.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
     document.getElementById("batimentos").appendChild(img);//Adiciona imagem como filha do id=batimentos.
@@ -32,25 +33,42 @@ function batimentos () {
 }
 
 // Anos de Copa
-function copas() {
+
+function todasCopas() {
+    let anoCopa = 1930;
+    let anoLimite = 2018;
+    let copas = document.getElementById('copas');
+    
+    while(anoCopa <= anoLimite){
+        console.log(anoCopa);
+        //copas.textContent = "Em: " + anoCopa + " teve Copa do Mundo.";
+        //copas.insertAdjacentHTML("afterend", anoCopa)
+        anoCopa = anoCopa + 4;
+    }
+    
+}
+
+
+/*
+function todasCopas() {
     let anoCopa = 1930;
     let anoLimite = parseInt(prompt('Qual o ano limite na consulta?'))
     let copas = document.getElementById("copas");
+    var para = document.createElement('p');
+
     while(anoCopa <= anoLimite && anoCopa <= 2018){
         console.log(anoCopa);
         anoCopa = anoCopa + 4;
-        if (anoCopa = 1942 || anoCopa == 1946) {
-            var para = document.createElement('p');
+        if (anoCopa = 1942 && anoCopa == 1946) {
             para.textContent = "Em: " + anoCopa + " não teve Copa devido a Guerra Mundial.";
-            continue
         } else {
-            var para = document.createElement('p');
             para.textContent = "Em: " + anoCopa + " teve Copa do Mundo.";
         }
         copas.appendChild(para);
         break;
     }
 }
+*/
 
 //Anos de Olimpiada
 function olimpiadas(){
