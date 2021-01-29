2400ef2426989b3ffa4e224b89877ddcceff344c
Isaac <email.email.com>
2021-01-29 20:25:03 -0300

    Adicionado string no id copas

--- script.js
+++ script.js
@@ -19,13 +19,14 @@ function batimentos () {
     let calcBatimentos = 'Idade: ' + idade + '<br>Seu coração bateu: ' + diasdevida * 24 * 60 * 80 + " vezes em toda sua vida.";
     console.log(calcBatimentos);
     let batimentos = document.getElementById("batimentos");
-    batimentos.insertAdjacentHTML("afterend", calcBatimentos);
     
-    let img = document.createElement("img");
-    img.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
-    document.getElementById("batimentos").appendChild(img); //Adiciona imagem como filha do id=batimentos.
-    img.height = "100"; 
-    //document.getElementById("batimentos").style="background-color: #F6CECE";
+    let imgBatimentos = document.createElement("img");
+    imgBatimentos.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
+    document.getElementById("batimentos").appendChild(imgBatimentos); //Adiciona imagem como filha do id=batimentos.
+    //batimentos.insertAdjacentElement("afterend", imgBatimentos); //Igual a linha acima
+    imgBatimentos.height = "100";
+    batimentos.insertAdjacentHTML("afterend", calcBatimentos);
+    document.getElementById("batimentos").style="background-color: #000";
 }
 
 // Anos de Copa
@@ -42,61 +43,55 @@ function batimentos () {
     function copadoMundo (ano) {
         let anoInicial = 1930;
         let anoLimite = parseInt(prompt('Qual o ano limite para consultar os anos de Copa ?'));
-        
-        if (ano == anoInicial){
-            console.log("<h3>Anos de Copa entre " + anoInicial + " e " + anoLimite + " :</h3>");     
-            for (ano = anoInicial; ano <= anoLimite; ano = ano + 4) {
+        var copas = document.getElementById("copas");
+
+        if (ano == anoInicial)
+            for (ano = anoInicial; ano <= anoLimite; ano = ano + 4)
                 if (checkWar(ano) == true)
-                continue;
-                console.log(ano)
-                //mostra(ano);    
-            }
-            return ano;
-        }
-        
-        let anosdeCopa = ano;
-        let copa = document.getElementById("copas");
-        copa.insertAdjacentHTML("afterend", anosdeCopa);
-        let img = document.createElement("img");
-        img.src = "https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/ad7523a3d1a139039d9a33bdd76a9ddf.jpg";
-        copa.insertAdjacentElement("afterend", img);
-        img.height = "200";
-        
-    }    
+                    //console.log(ano); // nada
+                    continue;
+                    //console.log(ano); //nada
+                    var divCopa = document.createElement("div");
+                    divCopa.textContent = anoLimite + " STRING COPA "; //textContent= para todos Node Objetos
+                    //copaTeste.innerText = anoInicial + ": Teve Copa"; //innerText=apenas para HTML objetos
+                    copas.insertAdjacentText("afterend", divCopa.innerText);
+                    //return ano.textContent; => nada
 
-/*
-//'Anos de Copa entre' + anoInicial + ' e ' + anoLimite + ' : ';
-let para = document.createElement('hr');
-document.getElementById("copas").appendChild(para);        
-let para = document.createElement('p');
-para.textContent = "string";
-let copas = document.getElementById("copas");
-copas.insertAdjacentElement("afterend", para)
-console.log("teste para")
-let copas = document.getElementById("copas");
-copas.textContent = "Em: " + anoCopa + " teve Copa do Mundo.";
-var para = document.createElement('p');
-para.textContent = "Em: " + anoCopa + " não teve Copa devido a Guerra Mundial.";
-para.textContent = "Em: " + anoCopa + " teve Copa do Mundo.";
-copas.appendChild(para);
-console.log('olimpiadas')
-let element = document.createElement('p');
-element.innerText = "string teste";
-let olimpiadas = document.getElementById("olimpiadas");
-olimpiadas.textContent = "teste tes tes teste"
-olimpiadas.insertAdjacentHTML("afterend", element)
-console.log(element)
-*/
 
+        let imgCopa = document.createElement("img");
+        imgCopa.src = "https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/ad7523a3d1a139039d9a33bdd76a9ddf.jpg";
+        copas.insertAdjacentElement("afterend", imgCopa); // insere element IMG apos o fim do id=copas.
+        //document.getElementById("copas").appendChild(imgCopa); // igual a linha acima
+        imgCopa.height = "200";
+        
+        /*
+        ano <= anoLimite ? document.getElementById("copas").innerText = "Teve Copa: " + ano + '<br>':
+                    document.getElementById("copas").innerHTML = "Nao teve copa: " + ano;
+        copa.insertAdjacentHTML("afterend", ano);
+
+        var span = document.querySelector('span');
+        span.innerHTML = "1<br>2<br>3<br>4\n5\n6\n7\n8";
+        console.log(span.innerText); // breaks in first half
+        console.log(span.textContent); // breaks in second half
+        */
+    } 
+    
+    
 
 //Anos de Olimpiada
 function olimpiadas(){
     let element = document.createElement('div');
-        element.innerText = "string teste";
+        element.innerText = "Teste OLIMPIADAS";
     let olimpiada = document.getElementById("olimpiadas");
         olimpiada.insertAdjacentText("afterend", element.innerText);
         console.log(element.innerText);
 }
+/*
+document.getElementById("copas").appendChild(para);        
+let para = document.createElement('p');
+para.textContent = "Em: " + anoCopa + " não teve Copa devido a Guerra Mundial.";
+copas.insertAdjacentElement("afterend", para)
+*/
 
 //Comparativo combustiveis
 function combustiveis (gas){
