29867a56458c7386bdfd91a8ecb716c22095aa1f
Isaac <email.email.com>
2021-03-01 18:10:33 -0300

    Anos de copa no DOM pulando linha

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cores=["#086A87", "##0489B1", "#0B3B39", "#4e84bc", "#314250", "#0B3861"];
 
     let i=0;
-    let timer=setInterval(mudaCorFundo,5000);
+    let timer=setInterval(mudaCorFundo,10000);
 	function mudaCorFundo(){
 		document.body.style.backgroundColor=cores[i];
 		i++;
@@ -16,7 +16,7 @@ function batimentos () {
     console.log('Anos de vida: ' + idade);
     let diasdevida = idade * 365;
     console.log('Dias de vida: ' + diasdevida);
-    let calcBatimentos = 'Idade: ' + idade + '<br>Seu coração bateu: ' + diasdevida * 24 * 60 * 80 + " vezes em toda sua vida.";
+    let calcBatimentos = 'Idade: ' + idade + '<br>' + 'Seu coração bateu: ' + diasdevida * 24 * 60 * 80 + " vezes em toda sua vida.";
     console.log(calcBatimentos);
     let batimentos = document.getElementById("batimentos");
     
@@ -24,60 +24,48 @@ function batimentos () {
     imgBatimentos.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
     document.getElementById("batimentos").appendChild(imgBatimentos); //Adiciona imagem como filha do id=batimentos.
     //batimentos.insertAdjacentElement("afterend", imgBatimentos); //Igual a linha acima
-    imgBatimentos.height = "100";
+    imgBatimentos.height = "180";
     batimentos.insertAdjacentHTML("afterend", calcBatimentos);
     document.getElementById("batimentos").style="background-color: #000";
 }
 
 // Anos de Copa
+let copa;
+let anoInicial = 1930;
+let anoLimite = 2018;
+let buttonCopa = document.getElementById('copas')
+    buttonCopa.addEventListener("click", anodeCopa)
+
     function checkWar (ano){
         var houveGuerra = false;
             if (ano >= 1942 && ano <= 1946){                    
-                houveGuerra = true;
+                houveGuerra = true; //Caso for V executa o codigo abaixo:
                 console.log('Sem Copa.')
-                //mostra(ano + ': Não teve Copa')
+                copa += 'Em ' + ano + ': nao teve Copa devido a Guerra. \n';
             };
         return houveGuerra;
     };
 
-    function copadoMundo (ano) {
-        let anoInicial = 1930;
-        let anoLimite = parseInt(prompt('Qual o ano limite para consultar os anos de Copa ?'));
-        var copas = document.getElementById("copas");
-
-        if (ano == anoInicial)
-            for (ano = anoInicial; ano <= anoLimite; ano = ano + 4)
-                if (checkWar(ano) == true)
-                    //console.log(ano); // nada
-                    continue;
-                    //console.log(ano); //nada
-                    var divCopa = document.createElement("div");
-                    divCopa.textContent = anoLimite + " STRING COPA "; //textContent= para todos Node Objetos
-                    //copaTeste.innerText = anoInicial + ": Teve Copa"; //innerText=apenas para HTML objetos
-                    copas.insertAdjacentText("afterend", divCopa.innerText);
-                    //return ano.textContent; => nada
-
-
+    function anodeCopa () {
+        for (ano = anoInicial; ano <= anoLimite; ano += 4) {
+            if (checkWar(ano) == true)
+                continue
+                console.log('Em: ' + ano + ': Teve Copa.')
+                copa += 'Em ' + ano + ': Teve Copa. \n'; 
+        }
+            if (ano > 2020) {
+                    console.log('Em: ' + ano + ' : vai ter copa.')
+                    copa += 'Em ' + ano + ': vai ter Copa.';        
+            }
         let imgCopa = document.createElement("img");
         imgCopa.src = "https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/ad7523a3d1a139039d9a33bdd76a9ddf.jpg";
         copas.insertAdjacentElement("afterend", imgCopa); // insere element IMG apos o fim do id=copas.
-        //document.getElementById("copas").appendChild(imgCopa); // igual a linha acima
-        imgCopa.height = "200";
-        
-        /*
-        ano <= anoLimite ? document.getElementById("copas").innerText = "Teve Copa: " + ano + '<br>':
-                    document.getElementById("copas").innerHTML = "Nao teve copa: " + ano;
-        copa.insertAdjacentHTML("afterend", ano);
-
-        var span = document.querySelector('span');
-        span.innerHTML = "1<br>2<br>3<br>4\n5\n6\n7\n8";
-        console.log(span.innerText); // breaks in first half
-        console.log(span.textContent); // breaks in second half
-        */
+        //document.getElementById("copas").appendChild(imgCopa); // mesma coisa q a linha acima
+        imgCopa.height = "200";        
+    document.getElementById("copas").innerText += copa; //innerHTML nao pula a linha com \n
+        //Com innerText usar \n para pular a linha. textContent nao pula a linha
     } 
     
-    
-
 //Anos de Olimpiada
 function olimpiadas(){
     let element = document.createElement('div');
