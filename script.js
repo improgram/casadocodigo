33e1e5861cf01e81aa58c8d5acbdf96e83002ba2
Isaac <email.email.com>
2021-03-08 20:33:34 -0300

    background changed

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-let cores=["#086A87", "##0489B1", "#0B3B39", "#4e84bc", "#314250", "#0B3861"];
+let cores= 
+    document.body.style["background-image"] = "url(https://upload.wikimedia.org/wikipedia/commons/4/4b/Milky_Way_3.jpg)";
 
-    let i=0;
-    let timer=setInterval(mudaCorFundo,10000);
-	function mudaCorFundo(){
-		document.body.style.backgroundColor=cores[i];
-		i++;
-		if(i >= cores.length){
-			i=0;
-		}
-    }
+let i=0;
+let timer=setInterval(mudaCorFundo,10000);
+
+function mudaCorFundo(){
+	document.body.style.backgroundImage=cores[i]; //ou backgroundColor
+	i++;
+    if(i >= cores.length){
+		i=0;
+	}
+}
 
 // Calculo Batimentos
+let buttonBatimentos = document.getElementById("batimentos")
+    buttonBatimentos.addEventListener("click", batimentos)
+
 function batimentos () {
     let idade = prompt('Quantos anos voce tem ?');
     console.log('Anos de vida: ' + idade);
@@ -19,11 +24,11 @@ function batimentos () {
     let calcBatimentos = 'Idade: ' + idade + '<br>' + 'Seu coração bateu: ' + diasdevida * 24 * 60 * 80 + " vezes em toda sua vida.";
     console.log(calcBatimentos);
     let batimentos = document.getElementById("batimentos");
-    
+
     let imgBatimentos = document.createElement("img");
     imgBatimentos.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
     document.getElementById("batimentos").appendChild(imgBatimentos); //Adiciona imagem como filha do id=batimentos.
-    //batimentos.insertAdjacentElement("afterend", imgBatimentos); //Igual a linha acima
+    //batimentos.insertAdjacentElement("afterend", imgBatimentos); =igual= a linha acima
     imgBatimentos.height = "180";
     batimentos.insertAdjacentHTML("afterend", calcBatimentos);
     document.getElementById("batimentos").style="background-color: #000";
@@ -62,11 +67,13 @@ let buttonCopa = document.getElementById('copas')
         copas.insertAdjacentElement("afterend", imgCopa); // insere element IMG apos o fim do id=copas.
         //document.getElementById("copas").appendChild(imgCopa); // mesma coisa q a linha acima
         imgCopa.height = "200";        
-    document.getElementById("copas").innerText += copa; //innerHTML nao pula a linha com \n
-        //Com innerText usar \n para pular a linha. textContent nao pula a linha
+    document.getElementById("copas").innerText += copa;
+    //Com innerText usar \n para pular a linha. textContent e innerHTML nao pula a linha
     } 
     
 //Anos de Olimpiada
+let buttonOlimpiadas = document.getElementById('olimpiadas');
+    buttonOlimpiadas.addEventListener("click", olimpiadas);
 function olimpiadas(){
     let element = document.createElement('div');
         element.innerText = "Teste OLIMPIADAS";
@@ -74,21 +81,24 @@ function olimpiadas(){
         olimpiada.insertAdjacentText("afterend", element.innerText);
         console.log(element.innerText);
 }
-/*
-document.getElementById("copas").appendChild(para);        
-let para = document.createElement('p');
-para.textContent = "Em: " + anoCopa + " não teve Copa devido a Guerra Mundial.";
-copas.insertAdjacentElement("afterend", para)
-*/
 
 //Comparativo combustiveis
-function combustiveis (gas){
+let buttonCombustiveis = document.getElementById('combustiveis');
+    buttonCombustiveis.addEventListener("click", combustiveis);
+function combustiveis (){
     console.log('combustiveis')
 }
 
 //Media de Idade dos familiares
+let buttonIdade = document.getElementById('mediaIdade');
+    buttonIdade.addEventListener("click", mediaIdade);
 function mediaIdade(){
     console.log('media de idade')
 }
 
 //Acertar Numero pensado
+let buttonNumero = document.getElementById('adivinharNumero');
+    buttonNumero.addEventListener("click", numeroPensado);
+function numeroPensado() {
+    console.log('numero pensado:' )
+}
