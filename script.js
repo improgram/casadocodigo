b2da981531435cba77617db07824aa004675afbc
Isaac <email.email.com>
2021-04-08 20:10:19 -0300

    adicionado script olimpiadas

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function batimentos () {
 
     let imgBatimentos = document.createElement("img");
     imgBatimentos.src = "https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png";
-    imgBatimentos.height = "180";
+    imgBatimentos.height = "200";
 
     buttonBatimentos.insertAdjacentElement("beforeEnd", imgBatimentos);  //Adiciona imagem como filha do id=batimentos.
     //document.getElementById("batimentos").appendChild(imgBatimentos); //Igual a linha acima
@@ -20,7 +20,7 @@ function batimentos () {
 
 // Anos de Copa
 let copa = 'Todas as Copas \n';
-let anoLimite = 2018;
+let ultimaCopa = 2018;
 let buttonCopa = document.getElementById('copas')
     buttonCopa.addEventListener("click", anodeCopa)
 
@@ -35,7 +35,7 @@ let buttonCopa = document.getElementById('copas')
     };
 
     function anodeCopa () {
-        for (ano = 1930; ano <= anoLimite; ano += 4) {
+        for (ano = 1930; ano <= ultimaCopa; ano += 4) {
             if (checkWar(ano) == true)  
                 continue
                 copa += 'Em ' + ano + ': Teve Copa. \n';
@@ -48,8 +48,8 @@ let buttonCopa = document.getElementById('copas')
 
         let imgCopa = document.createElement("img");
         imgCopa.src = "https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/ad7523a3d1a139039d9a33bdd76a9ddf.jpg";
-        imgCopa.height = "260";
-        imgCopa.width = "520";
+        imgCopa.height = "200";
+        imgCopa.width = "340";
         buttonCopa.insertAdjacentElement("beforeEnd", imgCopa); // insere element IMG apos o fim do id=copas.
         //document.getElementById("copas").appendChild(imgCopa); IGUAL a mesma coisa que a linha acima.
         buttonCopa.insertAdjacentHTML("beforeEnd", copa);
@@ -57,19 +57,64 @@ let buttonCopa = document.getElementById('copas')
     } 
     
 //Anos de Olimpiada
+let olimpiadas = 'Todas as Olimpiadas \n';
+let ultimaOlimpiadas = 2016;
 let buttonOlimpiadas = document.getElementById('olimpiadas');
-    buttonOlimpiadas.addEventListener("click", olimpiadas);
+    buttonOlimpiadas.addEventListener("click", anodeOlimpiadas);
 
-function olimpiadas(){
+function checkWar2 (ano){
+    let pausaGuerra = false;
+    if (ano >= 1940 && ano <= 1946){
+        pausaGuerra = true; //Caso for V executa a condicao
+        console.log('Sem Olimpiadas.')
+        olimpiadas += 'Em ' + ano + ': interrupção devido a Guerra Mundial. \n';
+    };
+    return pausaGuerra;
+}
+    
+function anodeOlimpiadas(){
+    for(ano = 1896; ano <= ultimaOlimpiadas; ano += 4){
+        if(checkWar2(ano) == true)
+        continue
+        olimpiadas += 'Em ' + ano + ': Teve Olimpiadas. \n';
+        console.log('Em: ' + ano + ': Teve Olimpiadas.')
+    }
+
+    if(ano > 2019) {
+        console.log('Em: ' + ano + ' : vai ter Olimpiadas.')
+        olimpiadas += 'Em ' + ano + ': Interrupção devido a Covid-19.'
+    }
+
     let imgOlimpiadas = document.createElement('img');
     imgOlimpiadas.src = "https://onlympics.com.br/wp-content/uploads/2019/01/Onlympics-jogos-olimpicos-1200x554.png";
-    imgOlimpiadas.height = "220";
+    imgOlimpiadas.width = "340";
 
-    buttonOlimpiadas.insertAdjacentElement("afterend", imgOlimpiadas);
-    document.getElementById('olimpiadas').innerText;
-    console.log('teste olimpiadas');
-
+    buttonOlimpiadas.insertAdjacentElement("beforeEnd", imgOlimpiadas);//insere elemento
+    buttonOlimpiadas.insertAdjacentHTML("beforeEnd", olimpiadas);
 }
+
+//Tabuada
+let buttonTabuada = document.getElementById('tabuada');
+    buttonTabuada.addEventListener("click", tabuada);
+
+    let multi = 1;
+    while(multi <= 10){
+        console.log('1 Vezes ' + multi + " = " + 1 * multi)
+        multi = multi + 1
+    }
+
+    function tabuada (){
+        let numero = parseInt(document.getElementById('numero').value);
+        let resposta = document.getElementById('resposta');
+        let resultado = " ";
+        console.log('teste Tabuada')
+
+        for(let count = 1; count <= 10; count++)
+        resultado += numero + " X " + count + " = " + numero*count + "<br />";
+        resposta.innerHTML = resultado;
+    }
+
+
 
 //Comparativo combustiveis
 let buttonCombustiveis = document.getElementById('combustiveis');
